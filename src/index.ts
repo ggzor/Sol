9325ab6dbddc3c0c87cfe0dab245c65ef560ab6e
ggzor <30713864+ggzor@users.noreply.github.com>
2018-07-24 11:14:55 -0500

    Adding scene 1 first draft

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { div, h1, makeDOMDriver } from '@cycle/dom'
+import { makeDOMDriver, p, section } from '@cycle/dom'
 import { run } from '@cycle/run'
 import distanceInWordsStrict from 'date-fns/distance_in_words_strict'
 import * as es from 'date-fns/locale/es'
+import { Power3, TweenMax } from 'gsap';
 import filter from 'lodash/filter'
 import map from 'lodash/map'
+import 'materialize-css/sass/materialize.scss'
 import xs from 'xstream'
 import '../src/styles/default.scss'
 import '../src/styles/layout.scss'
@@ -22,7 +24,15 @@ function main() {
                 t => !t.startsWith('0')
             )
 
-            return div(map(texts, t => h1(t)))
+            const nodes = map(texts, (t, i) => {
+                const classes: Record<string, boolean> = {}
+
+                classes[`counter-${i + 1}`] = true
+
+                return p({ class: classes }, t)
+            })
+
+            return section({ class: { 'counters': true } }, nodes)
         })
     }
 }
@@ -32,3 +42,15 @@ const drivers = {
 }
 
 run(main, drivers)
+
+for (let i = 2; i <= 6; i++) {
+    TweenMax.to(`#sol${i}`, 3 + i * 1, {
+        y: '-=10',
+        ease: Power3.easeOut,
+        repeat: -1,
+        yoyo: true,
+        yoyoEase: Power3.easeOut
+    })
+}
+
+setTimeout(() => TweenMax.to('.counters', 1, { opacity: 0.8 }), 2000)
